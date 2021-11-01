375111ba7fbe2d6fc6e77dfd77101ea506098548
barhome <barhome@github.com>
2021-11-01 17:32:09 +0200

    chore: implementing backend with traces

--- board/reference-app/backend/app.py
+++ board/reference-app/backend/app.py
@@ -3,32 +3,85 @@ from flask import Flask, render_template, request, jsonify
 import pymongo
 from flask_pymongo import PyMongo
 
+from jaeger_client import Config
+from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
+from opentelemetry import trace
+from opentelemetry.exporter import jaeger
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+    ConsoleSpanExporter,
+    SimpleExportSpanProcessor,
+)
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    SimpleExportSpanProcessor(ConsoleSpanExporter())
+)
+
 app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+RequestsInstrumentor().instrument()
+
+
 
 app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 
 mongo = PyMongo(app)
 
+
+def init_tracer(service):
+    logging.getLogger('').handlers = []
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+
+    config = Config(
+        config={
+            'sampler': {
+                'type': 'const',
+                'param': 1,
+            },
+            'logging': True,
+        },
+        service_name=service,
+    )
+
+    # this call also sets opentracing.tracer
+    return config.initialize_tracer()
+
+tracer_hello = init_tracer('first-hello-service')
+tracer_api = init_tracer('second_api_service')
+tracer_star = init_tracer('third_star_service')
+
 @app.route('/')
 def homepage():
+    with tracer_hello.start_span('HelloWorldSpan') as span:
+        span.set_tag('helloworldtag','tagvalueforhelloworldtag')
+
     return "Hello World"
 
 
 @app.route('/api')
 def my_api():
-    answer = "something"
+    with tracer_api.start_span('ApiSpan') as span:
+        answer = "something"
+        span.set_tag('apitag','tagvalueforapi')
     return jsonify(repsonse=answer)
 
 @app.route('/star', methods=['POST'])
 def add_star():
-  star = mongo.db.stars
-  name = request.json['name']
-  distance = request.json['distance']
-  star_id = star.insert({'name': name, 'distance': distance})
-  new_star = star.find_one({'_id': star_id })
-  output = {'name' : new_star['name'], 'distance' : new_star['distance']}
-  return jsonify({'result' : output})
+    with tracer_star.startspan('AddStarSpan') as span: 
+        star = mongo.db.stars
+        name = request.json['name']
+        distance = request.json['distance']
+        star_id = star.insert({'name': name, 'distance': distance})
+        new_star = star.find_one({'_id': star_id })
+        output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+        span.set_tag('addstartag',output['name'])
+    return jsonify({'result' : output})
 
 if __name__ == "__main__":
     app.run()
