443e6524c72c489f83f741fd06942a89bea2704d
barhome <barhome@github.com>
2021-11-06 22:11:29 +0200

    test: test regitering  metrics

--- board/reference-app/frontend/app.py
+++ board/reference-app/frontend/app.py
@@ -3,9 +3,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 metrics.info('app_info', 'Application info', version='1.0.3')
-@app.route('/')
-def homepage():
-    return render_template("main.html")
+
 
 metrics.register_default(
     metrics.counter(
@@ -13,6 +11,10 @@ metrics.register_default(
         labels={'path': lambda: request.path}
     )
 )
+
+@app.route('/')
+def homepage():
+    return render_template("main.html")
 
 if __name__ == "__main__":
     app.run()
